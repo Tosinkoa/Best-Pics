10003473da6a945387bb201c9d218b70a717e105
Tosinkoa <90416105+Tosinkoa@users.noreply.github.com>
2022-07-11 15:22:29 +0100

    Yup

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -1,29 +1,28 @@
+import { Popover } from "@headlessui/react";
 import Link from "next/link";
 import { IoMdPhotos } from "react-icons/io";
 import { RiMovieFill } from "react-icons/ri";
 
-export default function Dropdown({ menuHandler, showMenu }) {
+export default function Dropdown() {
   return (
     <div className="mydropdown">
-      {!showMenu && (
-        <div className="ddheaderlist">
-          <div className="ddmenulist">
-            <Link href="/">
-              <a className="ddoneheaderlist" onClick={menuHandler}>
-                <IoMdPhotos className="ddicon" />
-                Photos
-              </a>
-            </Link>
+      <div className="ddheaderlist">
+        <div className="ddmenulist">
+          <Link href="/">
+            <a className="ddoneheaderlist">
+              <IoMdPhotos className="ddicon" />
+              Photos
+            </a>
+          </Link>
 
-            <Link href="/videos">
-              <a className="ddoneheaderlist" onClick={menuHandler}>
-                <RiMovieFill className="ddicon" />
-                Videos
-              </a>
-            </Link>
-          </div>
+          <Link href="/videos">
+            <a className="ddoneheaderlist">
+              <RiMovieFill className="ddicon" />
+              Videos
+            </a>
+          </Link>
         </div>
-      )}
+      </div>
     </div>
   );
 }
--- components/Header.js
+++ components/Header.js
@@ -3,26 +3,36 @@ import Menubar from "./Menubar";
 import { FaBars } from "react-icons/fa";
 import Dropdown from "./Dropdown";
 import { useState } from "react";
+import { Popover } from "@headlessui/react";
+import { IoMdPhotos } from "react-icons/io";
+import { RiMovieFill } from "react-icons/ri";
+import HeadlessTransition from "./HeadlessTransition";
 
 export default function Header() {
-  const [showMenu, setShowMenu] = useState(true);
-
-  const menuHandler = () => setShowMenu(!showMenu);
-
   return (
     <div className="headerbackground">
       <div className="header">
         <h1 className="headerlogo">
           <Link href="/">BESTPICS</Link>{" "}
         </h1>
-        <div className="headermenuicon">
-          <button onClick={menuHandler}>
-            <FaBars />
-          </button>
-        </div>
+
+        <Popover>
+          <Popover.Button>
+            <div className="headermenuicon">
+              <button>
+                <FaBars />
+              </button>
+            </div>
+          </Popover.Button>
+          <Popover.Panel>
+            <HeadlessTransition>
+              <Dropdown />
+            </HeadlessTransition>
+          </Popover.Panel>
+        </Popover>
         <div className="headerlist">
           <Menubar />
-          <Dropdown menuHandler={menuHandler} showMenu={showMenu} />
+          {/* <Dropdown menuHandler={menuHandler} showMenu={showMenu} /> */}
         </div>
       </div>
     </div>
